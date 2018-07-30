8ebf7b637142ea17d3afbbb809488ff7650e30db
root <root@codvice06.cafe24.com>
2018-07-30 17:25:31 +0900

    Server Deploy & SSL Certificate

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,27 +12,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        DB::table('conversations')->delete();
-//        DB::table('categories')->delete();
-//        DB::table('contents')->delete();
-//        DB::table('classifications')->delete();
-//        DB::table('edits')->delete();
-//        DB::table('roles')->delete();
-//        DB::table('admins')->delete();
-//        DB::table('role_admin')->delete();
-//        DB::table('notices')->delete();
-//        DB::table('faqs')->delete();
+        DB::table('conversations')->delete();
+        DB::table('categories')->delete();
+        DB::table('contents')->delete();
+        DB::table('classifications')->delete();
+        DB::table('edits')->delete();
+        DB::table('roles')->delete();
+        DB::table('admins')->delete();
+        DB::table('role_admin')->delete();
+        DB::table('notices')->delete();
+        DB::table('faqs')->delete();
         DB::table('inquires')->delete();
 
-//        $this->call(CategorySeeder::class);
-//        $this->call(ConversationSeeder::class);
-//        $this->call(ClassificationSeeder::class);
-//        $this->call(ContentsSeeder::class);
-//        $this->call(EditSeeder::class);
-//        $this->call(RoleTableSeeder::class);
-//        $this->call(AdminTableSeeder::class);
-//        $this->call(NoticeSeeder::class);
-//        $this->call(FAQSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(ConversationSeeder::class);
+        $this->call(ClassificationSeeder::class);
+        $this->call(ContentsSeeder::class);
+        $this->call(EditSeeder::class);
+        $this->call(RoleTableSeeder::class);
+        $this->call(AdminTableSeeder::class);
+        $this->call(NoticeSeeder::class);
+        $this->call(FAQSeeder::class);
         $this->call(InquirySeeder::class);
     }
 }
